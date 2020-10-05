9fe02b05e3e8233bffd7df5d684bb7f6551827c8
luzyla <luzyla@gmail.com>
2020-10-05 05:29:08 -0300

    Checkout: pulido en HTML, estilado en CSS y funciones en JS

--- main.js
+++ main.js
@@ -12,13 +12,28 @@ const articles = document.querySelectorAll(".product")
 const filtroPuntajes = document.querySelectorAll(".filtro-puntaje")
 // const articles = document.querySelectorAll(".product")
 
-// MODAL CIERRE DE COMPRA
+// CHECKOUT COMPRA
 const botonComprar = document.getElementById("boton-comprar")
 const botonVaciar = document.getElementById("boton-vaciar")
 const confirmarCompra = document.querySelector(".contenedor-confirmar-compra")
 const overlay2 = document.getElementById("fondo-overlay-modal")
 const botonSeguirComprando = document.getElementById("button-buying")
 const botonFinalizarCompra = document.getElementById("button-ends")
+const sumaProductos = document.getElementById("suma-productos")
+const valorSubtotal = document.getElementById("valor-subtotal")
+const conEnvio = document.getElementById("con-envio")
+const valorEnvio = document.getElementById("valor-envio")
+const conDescuento = document.getElementById("con-descuento")
+const valorDescuento = document.getElementById("valor-descuento")
+const conRecargo = document.getElementById("con-recargo")
+const valorRecargo = document.getElementById("valor-recargo")
+const sumaTotal = document.getElementById("suma-total")
+const valorTotal = document.getElementById("valor-total")
+
+const radioEfectivo = document.getElementById("efectivo-debito")
+const radioTarjeta = document.getElementById("tarjeta")
+const checkboxEnvio = document.getElementById("envio")
+const checkboxDescuento = document.getElementById("dto")
 
 
 // CARRITO MENÚ DESPLEGABLE
@@ -48,6 +63,39 @@ botonSeguirComprando.onclick = () => {
     overlay.classList.remove("mostrar")
 }
 
+// OPERACIONES DEL CHECKOUT
+radioEfectivo.oninput = () => {
+    conRecargo.classList.add("hidden")
+}
+
+radioTarjeta.oninput = () => {
+    conRecargo.classList.remove("hidden")
+    sumarRecargo()
+}
+
+const sumarRecargo = (sumaProductos, porcentaje) => {
+    porcentaje = 0.10
+    let recargo = sumaProductos * porcentaje
+    recargo.textContent = recargo
+}
+console.log(sumarRecargo())
+
+checkboxEnvio.oninput = () => {
+    conEnvio.classList.toggle("hidden")
+    valorEnvio.textContent = "100"
+}
+
+checkboxDescuento.oninput = () => {
+    conDescuento.classList.toggle("hidden")
+    sumarDescuento()
+}
+
+const sumarDescuento = (sumaProductos, porcentaje) => {
+    porcentaje = 0.15
+    let descuento = sumaProductos * porcentaje
+    valorEnvio.textContent = descuento
+}
+console.log(sumarDescuento())
 
 
 // FILTRO X CATEGORIA
